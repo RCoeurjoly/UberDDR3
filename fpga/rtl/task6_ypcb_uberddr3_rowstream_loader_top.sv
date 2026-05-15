63af179f66306bf1162bcd8ef61a1f0f14f398a4
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2026-05-15 10:00:33 +0200

    Align YPCB rowstream BIST address width

--- fpga/rtl/task6_ypcb_uberddr3_rowstream_loader_top.sv
+++ fpga/rtl/task6_ypcb_uberddr3_rowstream_loader_top.sv
@@ -547,6 +547,7 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
     .DLL_OFF(0),
     .WB_ERROR(0),
     .BIST_MODE(0),
+    .BIST_ADDR_BITS(0),
     .ECC_ENABLE(0)
   ) uberddr3 (
     .i_controller_clk(controller_clk),
