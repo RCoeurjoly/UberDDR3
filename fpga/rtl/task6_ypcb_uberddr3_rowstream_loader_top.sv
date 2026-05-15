cee0331907415b8eb48db771fb1442fc52b6b730
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2026-05-15 08:51:50 +0200

    Align YPCB fullbeat shell to calibrated clocks

--- fpga/rtl/task6_ypcb_uberddr3_rowstream_loader_top.sv
+++ fpga/rtl/task6_ypcb_uberddr3_rowstream_loader_top.sv
@@ -24,7 +24,7 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
   output wire        ddram_we_n
 );
   localparam logic [31:0] JTAG_DEBUG_MAGIC = 32'h54364a44;
-  localparam logic [7:0] JTAG_DEBUG_VERSION = 8'd64;
+  localparam logic [7:0] JTAG_DEBUG_VERSION = 8'd89;
   localparam int JTAG_COMMAND_WIDTH = 192;
   localparam logic [31:0] LOADER_COMMAND_MAGIC = 32'h33445244;
   localparam logic [7:0] LOADER_OP_WRITE_CHUNK = 8'h01;
@@ -50,9 +50,9 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
   wire ddr3_clk;
   wire ddr3_clk_90;
   wire ref_clk;
+  wire clk400_raw;
+  wire clk400_90_raw;
   wire clk100_raw;
-  wire clk100_90_raw;
-  wire clk25_raw;
   wire clk200_raw;
   wire pll_clkfb;
   wire mmcm_locked;
@@ -65,7 +65,7 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
 
   PLLE2_BASE #(
     .BANDWIDTH("OPTIMIZED"),
-    .CLKFBOUT_MULT(20),
+    .CLKFBOUT_MULT(16),
     .CLKFBOUT_PHASE(0.000),
     .CLKIN1_PERIOD(20.000),
     .CLKOUT0_DIVIDE(2),
@@ -77,7 +77,7 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
     .CLKOUT2_DIVIDE(8),
     .CLKOUT2_DUTY_CYCLE(0.500),
     .CLKOUT2_PHASE(0.000),
-    .CLKOUT3_DIVIDE(5),
+    .CLKOUT3_DIVIDE(4),
     .CLKOUT3_DUTY_CYCLE(0.500),
     .CLKOUT3_PHASE(0.000),
     .DIVCLK_DIVIDE(1),
@@ -85,9 +85,9 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
     .STARTUP_WAIT("FALSE")
   ) clock_pll (
     .CLKFBOUT(pll_clkfb),
-    .CLKOUT0(clk100_raw),
-    .CLKOUT1(clk100_90_raw),
-    .CLKOUT2(clk25_raw),
+    .CLKOUT0(clk400_raw),
+    .CLKOUT1(clk400_90_raw),
+    .CLKOUT2(clk100_raw),
     .CLKOUT3(clk200_raw),
     .CLKOUT4(),
     .CLKOUT5(),
@@ -99,17 +99,17 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
   );
 
   BUFG clk100_bufg (
-    .I(clk100_raw),
+    .I(clk400_raw),
     .O(ddr3_clk)
   );
 
   BUFG clk100_90_bufg (
-    .I(clk100_90_raw),
+    .I(clk400_90_raw),
     .O(ddr3_clk_90)
   );
 
   BUFG clk25_bufg (
-    .I(clk25_raw),
+    .I(clk100_raw),
     .O(controller_clk)
   );
 
@@ -456,8 +456,8 @@ module task6_ypcb_uberddr3_rowstream_loader_top #(
   end
 
   ddr3_top #(
-    .CONTROLLER_CLK_PERIOD(8_000),
-    .DDR3_CLK_PERIOD(2_000),
+    .CONTROLLER_CLK_PERIOD(10_000),
+    .DDR3_CLK_PERIOD(2_500),
     .ROW_BITS(ROW_BITS),
     .COL_BITS(COL_BITS),
     .BA_BITS(BA_BITS),
