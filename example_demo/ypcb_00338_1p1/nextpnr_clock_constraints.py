697825336a44cdbd717cc488e56a933fc399d3d1
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2026-05-19 11:59:50 +0200

    ypcb_00338_1p1 demo and nix flake

---
+++ example_demo/ypcb_00338_1p1/nextpnr_clock_constraints.py
@@ -0,0 +1,62 @@
+"""Clock constraints for YPCB UberDDR3 nextpnr builds.
+
+By default these match the AMD/Xilinx Kintex-7 external-memory-interface
+guidance used by the active YPCB shell:
+
+- ddr3_clk = 400 MHz
+- ddr3_clk_90 = 400 MHz
+- controller_clk = 100 MHz
+- ref_clk = 200 MHz
+
+Set YPCB_UBERDDR3_CLOCK_PROFILE=openxc7-333 for the YPCB
+OpenXC7 profile:
+
+- ddr3_clk = 333.333333 MHz
+- ddr3_clk_90 = 333.333333 MHz
+- controller_clk = 83.333333 MHz
+- ref_clk = 200 MHz
+"""
+
+import os
+
+
+def env_mhz(name, default):
+    return float(os.environ.get(name, str(default)))
+
+
+profile = os.environ.get("YPCB_UBERDDR3_CLOCK_PROFILE", "default")
+
+if profile == "openxc7-333":
+    default_controller_clk_mhz = 83.333333
+    default_ddr3_clk_mhz = 333.333333
+    default_ref_clk_mhz = 200.0
+else:
+    default_controller_clk_mhz = 100.0
+    default_ddr3_clk_mhz = 400.0
+    default_ref_clk_mhz = 200.0
+
+controller_clk_mhz = env_mhz(
+    "YPCB_UBERDDR3_CONTROLLER_CLK_MHZ", default_controller_clk_mhz
+)
+ddr3_clk_mhz = env_mhz("YPCB_UBERDDR3_DDR3_CLK_MHZ", default_ddr3_clk_mhz)
+ref_clk_mhz = env_mhz("YPCB_UBERDDR3_REF_CLK_MHZ", default_ref_clk_mhz)
+clk100_raw_mhz = env_mhz("YPCB_UBERDDR3_CLK100_RAW_MHZ", controller_clk_mhz)
+clk400_raw_mhz = env_mhz("YPCB_UBERDDR3_CLK400_RAW_MHZ", ddr3_clk_mhz)
+clk400_90_raw_mhz = env_mhz("YPCB_UBERDDR3_CLK400_90_RAW_MHZ", ddr3_clk_mhz)
+
+# These names are taken from nextpnr clock reports for boot-clean YPCB
+# UberDDR3 builds. Missing names are harmless: nextpnr logs a
+# warning and ignores that specific constraint.
+for name, mhz in [
+    ("controller_clk", controller_clk_mhz),
+    ("clk100_raw", clk100_raw_mhz),
+    ("clk400_raw", clk400_raw_mhz),
+    ("clk400_90_raw", clk400_90_raw_mhz),
+    ("ddr3_clk", ddr3_clk_mhz),
+    ("ddr3_clk_90", ddr3_clk_mhz),
+    ("clk200_raw", ref_clk_mhz),
+    ("ref_clk", ref_clk_mhz),
+]:
+    ctx.addClock(name, mhz)
+    ctx.addClock("bist_top." + name, mhz)
+    ctx.addClock("calib_top." + name, mhz)
