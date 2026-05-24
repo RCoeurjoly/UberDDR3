14f9749cccc665e6c39595958a6b78c6ffeac347
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2026-05-24 10:07:42 +0200

    Add o_bist_counts output

--- rtl/ddr3_top.v
+++ rtl/ddr3_top.v
@@ -120,6 +120,7 @@ module ddr3_top #(
         // Debug outputs
         output wire[31:0] o_debug1,
         output wire[63:0] o_debug8,
+        output wire[63:0] o_bist_counts,
 //        output wire[31:0] o_debug2,
 //        output wire[31:0] o_debug3,
 //        output wire[(DQ_BITS*BYTE_LANES)/8-1:0] o_ddr3_debug_read_dqs_p,
@@ -330,6 +331,7 @@ ddr3_top #(
             // Debug outputs
             .o_debug1(o_debug1),
             .o_debug8(o_debug8),
+            .o_bist_counts(o_bist_counts),
 //            .o_debug2(o_debug2),
 //            .o_debug3(o_debug3)
             // User enabled self-refresh
